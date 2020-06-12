c02fb369d015365e8ef72c7ee0c40a9a03dea067
Gleb <43728100+lounres@users.noreply.github.com>
2020-06-13 00:36:08 +0300

    ORC

--- client.js
+++ client.js
@@ -14,7 +14,7 @@ const wrap = new (require("./classes/client_wrapper").Wrapper)(
         roomKey: argv.key,
         usersNumber: argv.num,
         username: argv.name,
-        // roomSettings: require("./roomConfig.json");
+        // roomSettings: require("./roomConfig.json"),
         webConfigs: {
             // protocol: "https:",
             // hostname: "m20-sch57.site",
